d74ceaac61576ee98e3332b52b4e2dd3eab2d2be
ChewyMochi <austinchen237@gmail.com>
2025-08-20 17:18:15 +0800

    Add Exit

--- src/main/java/MochiBot.java
+++ src/main/java/MochiBot.java
@@ -1,14 +1,19 @@
 public class MochiBot {
     public static void main(String[] args) {
         greet();
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("______________________________________________");
+        exit();
     }
 
     public static void greet() {
         System.out.println("______________________________________________");
         System.out.println("Hello! I'm MochiBot.");
         System.out.println("What can I do for you?");
+        System.out.println("______________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("______________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
         System.out.println("______________________________________________");
     }
 }
