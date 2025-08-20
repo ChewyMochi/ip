92699231d07fe765d51a4a6fd7b3418d8381ee53
ChewyMochi <austinchen237@gmail.com>
2025-08-20 17:04:05 +0800

    Rename Duke.java to MochiBot.java

---
+++ src/main/java/MochiBot.java
@@ -0,0 +1,9 @@
+public class MochiBot {
+    public static void main(String[] args) {
+        System.out.println("______________________________________________");
+        System.out.println("Hello! I'm MochiBot.\nWhat can I do for you?");
+        System.out.println("______________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("______________________________________________");
+    }
+}
