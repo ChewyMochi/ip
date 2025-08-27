99088801e74ebe32eb32c246b5bacbfd920d2ebc
ChewyMochi <austinchen237@gmail.com>
2025-08-28 00:18:30 +0800

    Create UnmarkCommand class

---
+++ src/main/java/UnmarkCommand.java
@@ -0,0 +1,18 @@
+public class UnmarkCommand extends Command {
+    private final int taskIndex;
+
+    public UnmarkCommand(int taskIndex) {
+        this.taskIndex = taskIndex;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui) throws MochiBotException {
+        if (this.taskIndex >= tasks.getSize()) {
+            throw new MochiBotException.InvalidTaskIndexException();
+        }
+        Task task = tasks.getTask(this.taskIndex);
+        task.markNotDone();
+        ui.printUnmarkTask(task);
+        Storage.saveTaskList(tasks);
+    }
+}
