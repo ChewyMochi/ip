2d88d7b26fa4a5eeb489ff943520eee8cb63d1e0
ChewyMochi <austinchen237@gmail.com>
2025-08-27 17:54:17 +0800

    Create Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,14 @@
+public class Ui {
+    public void printGreeting() {
+        System.out.println("______________________________________________");
+        System.out.println("Hello! I'm MochiBot ( '3')9.");
+        System.out.println("What can I do for you?");
+        System.out.println("______________________________________________");
+    }
+
+    public void printExit() {
+        System.out.println("______________________________________________");
+        System.out.println("Bye! Hope to see you again soon ( ;-;)>.");
+        System.out.println("______________________________________________");
+    }
+}
