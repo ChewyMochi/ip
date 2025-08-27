f6502475c6be2cc41f9065e1248727ac3359527b
ChewyMochi <austinchen237@gmail.com>
2025-08-28 01:51:10 +0800

    Fix greeting message typo

--- src/main/java/mochibot/ui/Ui.java
+++ src/main/java/mochibot/ui/Ui.java
@@ -18,7 +18,7 @@ public class Ui {
 
     public void printGreeting() {
         System.out.println("______________________________________________");
-        System.out.println("Hello! I'm MochiBot.MochiBot ( '3')9.");
+        System.out.println("Hello! I'm MochiBot. ( '3')9.");
         System.out.println("What can I do for you?");
         System.out.println("______________________________________________");
     }
