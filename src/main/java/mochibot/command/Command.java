f41848d3dc6dbd9d93fcf3e8303f7b5958e3f415
ChewyMochi <austinchen237@gmail.com>
2025-08-30 01:03:38 +0800

    Add JavaDocs for Command class

--- src/main/java/mochibot/command/Command.java
+++ src/main/java/mochibot/command/Command.java
@@ -6,6 +6,11 @@ import mochibot.task.TaskList;
 
 import mochibot.ui.Ui;
 
+/**
+ * This abstract class provides the abstract method {@code execute} for
+ * child classes to implement, and {@code hasExit} method to indicate the user
+ * exiting the program.
+ */
 public abstract class Command {
 
     public abstract void execute(TaskList tasks, Ui ui) throws MochiBotException;
