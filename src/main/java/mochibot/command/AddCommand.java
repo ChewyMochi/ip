34c77796450a0936cfc483229e74c0cf207c4a7d
ChewyMochi <austinchen237@gmail.com>
2025-08-30 01:03:25 +0800

    Modify JavaDocs for AddCommand class

--- src/main/java/mochibot/command/AddCommand.java
+++ src/main/java/mochibot/command/AddCommand.java
@@ -8,8 +8,13 @@ import mochibot.task.TaskList;
 import mochibot.ui.Ui;
 
 /**
- * This class represents an Add command, providing the method to add tasks
- * into the task list.
+ * <p>
+ *     This class represents an Add command, providing the method {@code execute}
+ *     to add tasks into the task list.
+ * </p>
+ * <p>
+ *     The constructor for the {@code AddCommand} class takes in a {@link Task} object.
+ * </p>
  */
 public class AddCommand extends Command {
     private final Task task;
@@ -19,11 +24,11 @@ public class AddCommand extends Command {
     }
 
     /**
-     * Executes the {@link AddCommand} object to add tasks to the task list,
-     * saves the task list to local text file and displays message
+     * Executes the {@code AddCommand} object to add tasks to the task list,
+     * saves the task list to local text file, and displays message
      * indicating task being added.
      *
-     * @param tasks {@link TaskList} object storing a list of added tasks.
+     * @param tasks {@link TaskList} object that stores all tasks.
      * @param ui {@link Ui} object to print displays.
      */
     @Override
