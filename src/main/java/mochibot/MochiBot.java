b8d56c7002a333ad92f6e114cb588cf0ce682b37
ChewyMochi <austinchen237@gmail.com>
2025-09-03 23:50:17 +0800

    Modify MochiBot class to run using Gui

--- src/main/java/mochibot/MochiBot.java
+++ src/main/java/mochibot/MochiBot.java
@@ -6,15 +6,18 @@ import mochibot.command.Command;
 import mochibot.parser.Parser;
 import mochibot.storage.Storage;
 import mochibot.task.TaskList;
+import mochibot.ui.Gui;
 import mochibot.ui.Ui;
 
 public class MochiBot {
     // public static ArrayList<MochiBot.Task> taskList = new ArrayList<>(100);
     private TaskList tasks;
     private final Ui ui;
+    private final Gui gui;
 
     public MochiBot() {
         ui = new Ui();
+        gui = new Gui();
         try {
             tasks = Storage.loadTaskList();
         } catch (FileNotFoundException e) {
@@ -25,13 +28,13 @@ public class MochiBot {
 
     public void run() {
         ui.printGreeting();
-        boolean hasExit = false;
-        while (!hasExit) {
+        boolean isExit = false;
+        while (!isExit) {
             try {
                 String fullCommand = ui.readCommand();
                 Command c = Parser.parse(fullCommand);
                 c.execute(tasks, ui);
-                hasExit = c.hasExit();
+                isExit = c.hasExit();
             } catch (MochiBotException e) {
                 ui.printErrorMessage(e.getMessage());
             }
@@ -40,5 +43,20 @@ public class MochiBot {
 
     public static void main(String[] args) {
         new MochiBot().run();
+    }
+
+    public String displayGreeting() {
+        return gui.displayGreeting();
+    }
+
+    public String getResponse(String input) {
+        String response;
+        try {
+            Command c = Parser.parse(input);
+            response = c.execute(tasks, gui);
+        } catch (MochiBotException e) {
+            response = gui.displayErrorMessage(e.getMessage());
+        }
+        return response;
     }
 }
