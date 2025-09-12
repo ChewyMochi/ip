08b66fc8ed691d3629b8b3d4fa15cc5cb274caf1
ChewyMochi <austinchen237@gmail.com>
2025-09-12 22:17:38 +0800

    Remove unused code
    Remove code that was previously used for the Ui, as it is now obsolete.

--- src/main/java/mochibot/MochiBot.java
+++ src/main/java/mochibot/MochiBot.java
@@ -7,16 +7,12 @@ import mochibot.parser.Parser;
 import mochibot.storage.Storage;
 import mochibot.task.TaskList;
 import mochibot.ui.Gui;
-import mochibot.ui.Ui;
 
 public class MochiBot {
-    // public static ArrayList<MochiBot.Task> taskList = new ArrayList<>(100);
     private TaskList tasks;
-    private final Ui ui;
     private final Gui gui;
 
     public MochiBot() {
-        ui = new Ui();
         gui = new Gui();
         try {
             tasks = Storage.loadTaskList();
@@ -24,25 +20,6 @@ public class MochiBot {
             System.out.println("Scanner cannot find file to read.");
             tasks = new TaskList();
         }
-    }
-
-    public void run() {
-        ui.printGreeting();
-        boolean isExit = false;
-        while (!isExit) {
-            try {
-                String fullCommand = ui.readCommand();
-                Command c = Parser.parse(fullCommand);
-                c.execute(tasks, ui);
-                isExit = c.hasExit();
-            } catch (MochiBotException e) {
-                ui.printErrorMessage(e.getMessage());
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        new MochiBot().run();
     }
 
     public String displayGreeting() {
